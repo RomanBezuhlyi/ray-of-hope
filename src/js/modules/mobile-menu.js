e30955ebdb94132d590b26dbd2093f6f2d6a17b9
Роман Безуглий <romanbezuhlyi@MacBook-Pro-14.local>
2025-05-06 15:11:04 +0300

    added empty cart

--- src/js/modules/mobile-menu.js
+++ src/js/modules/mobile-menu.js
@@ -2,12 +2,24 @@ document.addEventListener('DOMContentLoaded', function () {
 	const burgerButton = document.querySelector('.header__right-burger')
 	const mobileNav = document.querySelector('.mobile-nav')
 	const body = document.body
+	const mobileNavLinks = document.querySelectorAll('.mobile-nav__list a')
+
+	function closeMobileMenu() {
+		mobileNav.classList.remove('active')
+		burgerButton.classList.remove('active')
+		body.style.overflow = ''
+	}
 
 	burgerButton.addEventListener('click', function () {
 		const isActive = mobileNav.classList.contains('active')
-
 		mobileNav.classList.toggle('active')
 		burgerButton.classList.toggle('active')
 		body.style.overflow = isActive ? '' : 'hidden'
+	})
+
+	mobileNavLinks.forEach(link => {
+		link.addEventListener('click', function () {
+			closeMobileMenu()
+		})
 	})
 })
